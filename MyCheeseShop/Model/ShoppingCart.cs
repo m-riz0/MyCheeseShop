798da93529e1916864c2f9b3ebdbeb7ef76ebebc
Moboss2k05 <mobarizmirza.rizwan@gmail.com>
2024-04-26 10:29:35 +0100

    slide 27. need to get layout fixed and all buttons working

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -2,6 +2,46 @@
 {
     public class ShoppingCart
     {
-        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public event Action? OnCartUpdated;
+        public List<CartItem> _items;
+
+        public ShoppingCart()
+        {
+            _items = [];
+        }
+
+        public void AddItem(Cheese cheese, int quantity)
+        {
+            var item  = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if (item == null)
+                _items.Add(new CartItem { Cheese = cheese, Quantity = quantity });
+            else
+                item.Quantity = quantity;
+
+            OnCartUpdated?.Invoke();
+        }
+
+        public IEnumerable<CartItem> GetItems()
+        {
+            return _items;
+        }
+
+        public void RemoveItem(Cheese cheese)
+        {
+            _items.RemoveAll(item => item.Cheese.Id == cheese.Id);
+            OnCartUpdated?.Invoke();
+        }
+
+        public void RemoveItem(Cheese cheese, int quantity)
+        {
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if (item is not null)
+            {
+                item.Quantity -= quantity;
+                if (item.Quantity <= 0)
+                    _items.Remove(item);
+            }
+            OnCartUpdated?.Invoke();
+        }
     }
 }
