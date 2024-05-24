82d4dc7fb58ba2d8de7b3a675a403a018f5ada52
Moboss2k05 <mobarizmirza.rizwan@gmail.com>
2024-05-24 10:24:23 +0100

    Finished AllOrders page.

---
+++ MyCheeseShop/wwwroot/js/app.js
@@ -0,0 +1,17 @@
+﻿function addTooltips() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+}
+
+function removeTooltips() {
+    var tooltipElements = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipElements.forEach(function (element) {
+        var tooltip = bootstrap.Tooltip.getInstance(element);
+        if (tooltip) {
+            tooltip.dispose();
+        }
+    });
+}
